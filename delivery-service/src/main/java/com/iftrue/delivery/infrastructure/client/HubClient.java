832abbeefdb74e3bce0ea8d9d13d985ac068cee7
taeeonkim <myhom43@gmail.com>
2026-08-10 11:24:18 +0900

    feat: 동일 허브 루트 생성 로직 추가 및 배송 중복 검증 기능 구현

--- delivery-service/src/main/java/com/iftrue/delivery/infrastructure/client/HubClient.java
+++ delivery-service/src/main/java/com/iftrue/delivery/infrastructure/client/HubClient.java
@@ -1,16 +1,19 @@
 package com.iftrue.delivery.infrastructure.client;
 
+import com.iftrue.delivery.global.common.ApiResponse;
 import com.iftrue.delivery.infrastructure.client.dto.HubRouteResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.UUID;
+
 @FeignClient(name = "hub-service")
 public interface HubClient {
 
     @GetMapping("/api/v1/internal/hub-routes/path")
-    HubRouteResponse getShortestRoute(
-            @RequestParam("departureHubId") String departureHubId,
-            @RequestParam("arrivalHubId") String arrivalHubId
+    ApiResponse<HubRouteResponse> getShortestRoute(
+            @RequestParam("departureHubId") UUID departureHubId,
+            @RequestParam("arrivalHubId") UUID arrivalHubId
     );
 }
