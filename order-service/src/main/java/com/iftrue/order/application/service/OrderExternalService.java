d97b0ffb1eb1a6d1c18155e7c95eb8391a9d2fe6
wontae <dnjsxo0616@gmail.com>
2026-08-10 21:48:25 +0900

    feat: 주문 외부 서비스 호출 계층 구현 #47

---
+++ order-service/src/main/java/com/iftrue/order/application/service/OrderExternalService.java
@@ -0,0 +1,55 @@
+package com.iftrue.order.application.service;
+
+import com.iftrue.order.infrastructure.client.company.CompanyClient;
+import com.iftrue.order.infrastructure.client.company.dto.CompanyResponse;
+import com.iftrue.order.infrastructure.client.delivery.DeliveryClient;
+import com.iftrue.order.infrastructure.client.delivery.dto.DeliveryCreateRequest;
+import com.iftrue.order.infrastructure.client.product.ProductClient;
+import com.iftrue.order.infrastructure.client.product.dto.InventoryQuantityRequest;
+import com.iftrue.order.infrastructure.client.product.dto.ProductResponse;
+import com.iftrue.order.infrastructure.client.user.UserClient;
+import com.iftrue.order.infrastructure.client.user.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderExternalService {
+
+    private final CompanyClient companyClient;
+    private final ProductClient productClient;
+    private final UserClient userClient;
+    private final DeliveryClient deliveryClient;
+
+    public CompanyResponse getCompany(UUID companyId) {
+        return companyClient.getCompany(companyId);
+    }
+
+    public ProductResponse getProduct(UUID productId) {
+        return productClient.getProduct(productId);
+    }
+
+    public UserResponse getRecipient(UUID userId) {
+        return userClient.getUser(userId);
+    }
+
+    public void decreaseInventory(UUID orderId, UUID productId, int quantity
+    ) {
+        InventoryQuantityRequest request = new InventoryQuantityRequest(orderId, quantity);
+
+        productClient.decreaseInventory(productId, request);
+    }
+
+    public void restoreInventory(UUID orderId, UUID productId, int quantity
+    ) {
+        InventoryQuantityRequest request = new InventoryQuantityRequest(orderId, quantity);
+
+        productClient.restoreInventory(productId, request);
+    }
+
+    public void createDelivery(DeliveryCreateRequest request) {
+        deliveryClient.createDelivery(request);
+    }
+}
