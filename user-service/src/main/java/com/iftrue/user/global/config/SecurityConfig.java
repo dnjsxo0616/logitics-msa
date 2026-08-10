a716604c02a9714c02e15001be9fba51b718e1fb
juwonyoung <dnjsdud0702@naver.com>
2026-08-10 14:54:59 +0900

    feat: security 추가

--- user-service/src/main/java/com/iftrue/user/global/config/SecurityConfig.java
+++ user-service/src/main/java/com/iftrue/user/global/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.iftrue.user.global.config;
 
 
-import com.iftrue.user.global.security.InternalAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
