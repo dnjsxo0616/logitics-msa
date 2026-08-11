682ea3bae99d8363dff54264af2293d7aa7df569
wontae <dnjsxo0616@gmail.com>
2026-08-11 17:13:59 +0900

    feat: Notification 공통 응답 및 예외 처리 구현 #52

---
+++ notification-service/src/main/java/com/iftrue/notification/global/response/ApiResponse.java
@@ -0,0 +1,38 @@
+package com.iftrue.notification.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.Instant;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private Instant timestamp;
+    private int status;
+    private String code;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return of(HttpStatus.OK, data);
+    }
+
+    public static <T> ApiResponse<T> created(T data) {
+        return of(HttpStatus.CREATED, data);
+    }
+
+    public static <T> ApiResponse<T> accepted(T data) {
+        return of(HttpStatus.ACCEPTED, data);
+    }
+
+    private static <T> ApiResponse<T> of(HttpStatus status, T data) {
+        return new ApiResponse<>(
+                Instant.now(),
+                status.value(),
+                "success",
+                data
+        );
+    }
+}
