58062f3d56057c27fe984e3c2c6cd1d6db467006
stephangod <sdm331224@gmail.com>
2026-08-13 11:27:43 +0900

    ci 에러 수정

--- product-service/src/main/java/com/if_true/product/infrastructure/client/CompanyClient.java
+++ product-service/src/main/java/com/if_true/product/infrastructure/client/CompanyClient.java
@@ -5,10 +5,16 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "company-service")
 public interface CompanyClient {
 
 	@GetMapping("/api/v1/companies/{companyId}")
-	CompanyResponse getCompany(@PathVariable UUID companyId);
+	CompanyResponse getCompany(
+		@PathVariable UUID companyId,
+		@RequestHeader("X-Gateway-Secret") String gatewaySecret,
+		@RequestHeader("X-User-Id") String userId,
+		@RequestHeader("X-User-Role") String userRole
+	);
 }
