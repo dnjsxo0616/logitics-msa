cad1f210f02bfef2ff1d53db53072aabc8722ba6
stephangod <sdm331224@gmail.com>
2026-08-13 10:49:51 +0900

    timestamp -> timestapmz로 수정

--- product-service/src/main/java/com/if_true/product/infrastructure/client/dto/ApiResponse.java
+++ product-service/src/main/java/com/if_true/product/infrastructure/client/dto/ApiResponse.java
@@ -3,7 +3,7 @@ package com.if_true.product.infrastructure.client.dto;
 import java.time.Instant;
 
 public record ApiResponse<T>(
-	Instant timestamp,
+	Instant timestampz,
 	int status,
 	String code,
 	T data
