4011ffaa28d961c31bd0031c0555e9b3ecea402c
juwonyoung <dnjsdud0702@naver.com>
2026-08-13 11:32:20 +0900

    fix: user 내부 조회 필드 추가

--- user-service/src/main/java/com/iftrue/user/application/InternalUserService.java
+++ user-service/src/main/java/com/iftrue/user/application/InternalUserService.java
@@ -21,10 +21,15 @@ public class InternalUserService {
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
         return new InternalUserResponse(
-                user.getCompanyId(),
+                user.getId(),
                 user.getUsername(),
+                user.getNickname(),
                 user.getEmail(),
-                user.getSlackId()
+                user.getSlackId(),
+                user.getRole(),
+                user.getHubId(),
+                user.getCompanyId(),
+                user.getStatus()
         );
     }
 }
--- user-service/src/main/java/com/iftrue/user/presentation/response/InternalUserResponse.java
+++ user-service/src/main/java/com/iftrue/user/presentation/response/InternalUserResponse.java
@@ -1,12 +1,20 @@
 package com.iftrue.user.presentation.response;
 
 
+import com.iftrue.user.domain.UserRole;
+import com.iftrue.user.domain.UserStatus;
+
 import java.util.UUID;
 
 public record InternalUserResponse(
-        UUID companyId,
-        String name,
+        UUID id,
+        String username,
+        String nickname,
         String email,
-        String slackId
+        String slackId,
+        UserRole role,
+        UUID hubId,
+        UUID companyId,
+        UserStatus status
 ) {
 }
