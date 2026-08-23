5a93196a1041306dda0dd99e3429c0f926f3e6f1
wontae <dnjsxo0616@gmail.com>
2026-08-23 15:24:16 +0900

    fix: Notification 로컬 실행 설정 정리
    - 로컬 PostgreSQL 기본 포트를 공유 DB에 맞게 5432로 변경
    - 동기 처리 구조에서 사용하지 않는 스케줄링 활성화 제거

--- notification-service/src/main/java/com/iftrue/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/iftrue/notification/NotificationServiceApplication.java
@@ -2,9 +2,7 @@ package com.iftrue.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class NotificationServiceApplication {
 
